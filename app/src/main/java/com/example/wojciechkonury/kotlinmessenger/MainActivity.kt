f6339aa896e2dd40580e085c993961634755768f
wojtek717 <wojciechkonury@gmail.com>
2019-02-18 11:36:37 +0100

    Register screen UI + start LoginActivity

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.wojciechkonury.kotlinmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email: " + email)
+            Log.d("MainActivity", "Password: " + password)
+        }
+
+        already_have_account_text_view_register.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
