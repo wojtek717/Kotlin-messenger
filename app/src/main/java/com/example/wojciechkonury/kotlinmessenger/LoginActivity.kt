ad7869eec590b5c07e01a4eedf10558821f0f8a5
wojtek717 <wojciechkonury@gmail.com>
2019-02-18 17:00:26 +0100

    Firebase implemetation + registration

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.example.wojciechkonury.kotlinmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener{
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("LoginActivity", "Attempt to login \nemail: " + email + "\npassword: " + password)
+        }
+
+        backtoregistration_textview_login.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,20 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("MainActivity", "Email: " + email)
             Log.d("MainActivity", "Password: " + password)
+
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Email or password is empty", Toast.LENGTH_SHORT).show()
+
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    //else if Successful
+                    Log.d("MainActivity", "Successfully created user with uid: " + it.result.user.uid)
+                }
         }
 
         already_have_account_text_view_register.setOnClickListener {
