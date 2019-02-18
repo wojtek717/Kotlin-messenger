e2c02aa5e56c39c1ec34fc110bafeb1c6ef56974
wojtek717 <wojciechkonury@gmail.com>
2019-02-18 20:54:01 +0100

    Selecting photo in registration

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/RegisterActivity.kt
@@ -1,14 +1,17 @@
 package com.example.wojciechkonury.kotlinmessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +22,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         already_have_account_text_view_register.setOnClickListener {
-            Log.d("MainActivity", "Try to show login activity")
+            Log.d("RegisterActivity", "Try to show login activity")
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
 
-            //Notburningkeypls
+        selectphoto_button_register.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            val uri = data.data //where the image is stored
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver ,uri) //bitmap of selected photo
+            val bitmapDrawable = BitmapDrawable(bitmap) //drawable bitmap of selected photo
+
+            selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
         }
     }
 
