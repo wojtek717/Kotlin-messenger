e04d9f89c5c18de372be5168450ab635cc420eb3
wojtek717 <wojciechkonury@gmail.com>
2019-02-20 12:19:08 +0100

    Sign out button

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessagesActivity : AppCompatActivity() {
@@ -26,6 +27,27 @@ class LatestMessagesActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
+    }
+
+    //when user click button in menu
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        //check what button is clicked
+        when(item?.itemId){
+            R.id.menu_new_message -> {
+
+            }
+            R.id.menu_sign_out -> {
+                //log out
+                FirebaseAuth.getInstance().signOut()
+
+                //go back to registerActivity
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
