2d885a879f3c81fef77b57d9a34b2ae5fd8d517d
wojtek717 <wojciechkonury@gmail.com>
2019-02-20 12:13:32 +0100

    Navbar menu

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
@@ -1,12 +1,36 @@
 package com.example.wojciechkonury.kotlinmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessagesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
+
+        verifyUsersIsLoggedIn()
+    }
+
+    private fun verifyUsersIsLoggedIn()
+    {
+        val uid = FirebaseAuth.getInstance().uid
+
+        //check if user is logged in
+        if(uid == null)
+        {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
