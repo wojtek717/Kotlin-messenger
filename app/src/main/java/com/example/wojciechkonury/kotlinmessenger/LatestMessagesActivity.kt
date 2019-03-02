b3d263068afb9ff33e9e38533ebbc801d775a87e
wojtek717 <wojciechkonury@gmail.com>
2019-03-02 20:16:15 +0100

    Load User Info to LatestMessages

--- app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
+++ app/src/main/java/com/example/wojciechkonury/kotlinmessenger/LatestMessagesActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
@@ -134,7 +135,31 @@ class LatestMessagesActivity : AppCompatActivity() {
 
 class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.textView_username.text = chatMessage.fromId
+        val chatPartnerId: String
+
+        if (chatMessage.fromId == FirebaseAuth.getInstance().uid){
+            chatPartnerId = chatMessage.toId
+        }else{
+            chatPartnerId = chatMessage.fromId
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                val chatPartner = p0.getValue(User::class.java)
+                val profileImageTarger = viewHolder.itemView.imageView_latest_messages_row
+
+                viewHolder.itemView.textView_username.text = chatPartner?.username
+                Picasso.get().load(chatPartner?.profileImageUrl).into(profileImageTarger)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+
         viewHolder.itemView.textView_latest_message.text = chatMessage.text
     }
 
